552f0ed297df648815e850653f11477963ef7500
Lucas Alves <lucasalves94@gmail.com>
2023-06-17 15:57:11 -0300

    first commit

---
+++ LuspasBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,9 @@
+
+    public record CreateBreakfastRequest(
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDateTime,
+        List<string> Savory,
+        List<string> Sweet
+    );
