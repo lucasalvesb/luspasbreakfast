5b550d5c0e9d60d90839c6cd194f8324e4d86f97
Lucas Alves <lucasalves94@gmail.com>
2023-06-17 22:12:05 -0300

    last adjustments

--- LuspasBreakfast/Controllers/ApiController.cs
+++ LuspasBreakfast/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LuspasBreakfast.Controllers;
 
@@ -9,6 +10,23 @@ public class ApiController : ControllerBase
 {
   protected IActionResult Problem(List<Error> errors)
   {
+
+    if (errors.All(e => e.Type == ErrorType.Validation))
+    {
+      var modelStateDictionary = new ModelStateDictionary();
+
+      foreach (var error in errors)
+      {
+        modelStateDictionary.AddModelError(error.Code, error.Description);
+      }
+      return ValidationProblem(modelStateDictionary);
+    }
+
+    if (errors.Any(e => e.Type == ErrorType.Unexpected))
+    {
+      return Problem();
+    }
+
     var firstError = errors[0];
 
     var statusCode = firstError.Type switch
